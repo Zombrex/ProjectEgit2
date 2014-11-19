822d93e73a044579445654e0fbb58ebc9d798f73
Angel <Angel@jose>
2014-11-19 14:10:01 +0100

    Principal1.2

--- ProjectEgit2/src/Main.java
+++ ProjectEgit2/src/Main.java
@@ -7,6 +7,11 @@ public class Main {
 		 c = a + b; 
 		 return c; 
 		 }
+	public static int resta(int a, int b) { 
+		 int c; 
+		 c = a - b; 
+		 return c; 
+		 } 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
